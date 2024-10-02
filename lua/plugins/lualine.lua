cd523d7d88ae88c83215f8652201c43be10121a1
david.li <lidwc@digiwin.com>
2024-10-02 08:35:35 +0800

    lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,15 @@
 -- lualine.lua
+-- 在 lualine 中，显示区域被分成了 6 个部分，分被用 A,B,C,X,Y,Z 组成。
 return {
     {
         'nvim-lualine/lualine.nvim',
         config = function()
-            require('lualine').setup()
+            require('lualine').setup({
+	            options = {
+		        --theme = "auto",
+		        component_separators = { right = "|" },}
+
+            })
         end
     }
 }
